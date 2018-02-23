a7343a94318af8ad5f6847be34000d1735a8748c
Miko <mixeradamski@gmail.com>
2018-02-23 18:54:11 +0000

    .

--- week-3/reversecomp_031.py
+++ week-3/reversecomp_031.py
@@ -1,57 +1,23 @@
 import sys
 
-# def main():
-#     l = []
-#     ans = []
-#     words = [w.strip() for w in sys.stdin]
-#     for w in words:
-#         if len(w) >= 5:
-#             x = w[::-1]
-#             l.append(w)
-#     print(l)
-#     for w in l:
-#         if w == x:
-#             print(w)
-
 def main():
-    ans = []
     l = set()
+    x = set()
+    ans = []
     words = [w.strip() for w in sys.stdin]
     for w in words:
         if len(w) >= 5:
             l.add(w)
-    s = w
-    for c in l:
-        low = c.lower()
-        low = c[::-1]
-        cap = c.capitalize()
-        if low in l or cap in l:
-            ans.append(s)
-
-    for s in ans:
-        ans.append(w)
+    for w in l:
+        s = w
+        w = w[::-1].lower()
+        if w in l or w.capitalize() in l:
+            x.add(s)
+    for s in x:
+        ans.append(s)
     ans = sorted(ans, key=str.lower)
     print(ans)
 
-# def main():
-#     s = sys.stdin.readlines()
-#     a = set()
-#     p = set()
-#     r = []
-#     for line in s:
-#         if len(line.strip()) >= 5:
-#             a.add(line.strip())
-#     for c in a:
-#         f = c
-#         c = c.lower()
-#         c = c[::-1]
-#         d = c.capitalize()
-#         if c in a or d in a:
-#                 p.add(f)
-#     for f in p:
-#         r.append(f)
-#     r = sorted(r, key=str.lower)
-#     print (r)
 
 if __name__ == '__main__':
     main()
