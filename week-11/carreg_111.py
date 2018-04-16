1f1d2da9d115f4df777b97fea7ce81f74e169642
Miixer <33287805+Miixer@users.noreply.github.com>
2018-04-16 16:49:28 +0100

    .

--- week-11/carreg_111.py
+++ week-11/carreg_111.py
@@ -2,7 +2,7 @@ from re import findall
 import sys
 
 #counties = C|CE|CN|CW|D|DL|G|KE|KK|KY|L|LD|LH|LM|LS|MH|MN|MO|OY|RN|SO|T|W|WH|WX|WW
-car = r"\d{2}[12]\s(?:C|CE|CN|CW|D|DL|G|KE|KK|KY|L|LD|LH|LM|LS|MH|MN|MO|OY|RN|SO|T|W|WH|WX|WW)"
+car = r"\b\d{2}[12]\s(?:C|CE|CN|CW|D|DL|G|KE|KK|KY|L|LD|LH|LM|LS|MH|MN|MO|OY|RN|SO|T|W|WH|WX|WW)\s[1-9][0-9]{0,5}\b"
 
 def main():
    # Verify regular expression is not overly long
